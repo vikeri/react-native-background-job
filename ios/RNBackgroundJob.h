2cf3adbc7a8c8611cfaf2457bb3d00c775f2217d
Ruben Weijers <ruben@vita.io>
2017-02-08 17:40:51 +0100

    make RN 0.40 header backwards compatible

--- ios/RNBackgroundJob.h
+++ ios/RNBackgroundJob.h
@@ -1,5 +1,9 @@
 
-#import "<React/RCTBridgeModule.h>"
+#if __has_include("RCTBridgeModule.h")
+  #import "RCTBridgeModule.h"
+#else
+  #import <React/RCTBridgeModule.h>
+#endif
 
 @interface RNBackgroundJob : NSObject <RCTBridgeModule>
 
