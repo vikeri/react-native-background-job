54a6f52b283fd4ad741289d014350c268001c537
Hassane Moustapha <hassanemoustapha@gmail.com>
2017-02-21 08:51:46 +0000

    fixed the issue due to the headless production guard

--- android/src/main/java/com/pilloxa/backgroundjob/HeadlessService.java
+++ android/src/main/java/com/pilloxa/backgroundjob/HeadlessService.java
@@ -3,14 +3,18 @@ package com.pilloxa.backgroundjob;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.HeadlessJsTaskService;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
 /**
  * Created by viktor on 2016-12-13.
  */
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.HeadlessJsTaskService;
+import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.common.LifecycleState;
 
 public class HeadlessService  extends HeadlessJsTaskService {
     @Override
@@ -18,6 +22,26 @@ public class HeadlessService  extends HeadlessJsTaskService {
         Bundle extras = intent.getExtras();
         String jobKey = extras.getString("jobKey");
         int timeout = extras.getInt("timeout");
-        return new HeadlessJsTaskConfig( jobKey, Arguments.fromBundle(extras), timeout);
+
+        if( isAppInForeground() ){
+          stopSelf();
+          return null;
+        }
+        return new HeadlessJsTaskConfig(
+              jobKey,
+              Arguments.fromBundle(extras),
+              timeout
+            );
+    }
+
+    private boolean isAppInForeground() {
+        final ReactInstanceManager reactInstanceManager =
+                ((ReactApplication) getApplication())
+                        .getReactNativeHost()
+                        .getReactInstanceManager();
+        ReactContext reactContext =
+          reactInstanceManager.getCurrentReactContext();
+
+        return(reactContext != null && reactContext.getLifecycleState() == LifecycleState.RESUMED);
     }
 }
