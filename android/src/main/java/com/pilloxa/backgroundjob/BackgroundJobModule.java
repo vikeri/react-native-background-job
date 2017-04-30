c813975c5fb0ad2018cb4206c8b8f24d5ebd0022
cbjs <cbjs.deng@gmail.com>
2017-04-30 13:40:06 +0800

    add allowInForeground

--- android/src/main/java/com/pilloxa/backgroundjob/BackgroundJobModule.java
+++ android/src/main/java/com/pilloxa/backgroundjob/BackgroundJobModule.java
@@ -59,13 +59,14 @@ public class BackgroundJobModule extends ReactContextBaseJavaModule implements L
                          boolean requiresCharging,
                          boolean requiresDeviceIdle,
                          boolean alwaysRunning,
+                         boolean allowedInForeground,
                          String title,
                          String icon,
                          String text) {
         int taskId = jobKey.hashCode();
 
         Log.v(LOG_TAG,
-                String.format("Scheduling: %s, timeout: %s, period: %s, network type: %s, requiresCharging: %s, requiresDeviceIdle: %s, alwaysRunning: %s, notificationTitle: %s, notificationText %s, notificationIcon: %s",
+                String.format("Scheduling: %s, timeout: %s, period: %s, network type: %s, requiresCharging: %s, requiresDeviceIdle: %s, alwaysRunning: %s, allowedInForeground: %s, notificationTitle: %s, notificationText %s, notificationIcon: %s",
                         jobKey,
                         timeout,
                         period,
@@ -73,6 +74,7 @@ public class BackgroundJobModule extends ReactContextBaseJavaModule implements L
                         requiresCharging,
                         requiresDeviceIdle,
                         alwaysRunning,
+                        allowedInForeground,
                         title,
                         text,
                         icon));
@@ -92,6 +94,7 @@ public class BackgroundJobModule extends ReactContextBaseJavaModule implements L
         jobExtras.putInt("requiresCharging", requiresCharging ? 1 : 0);
         jobExtras.putInt("requiresDeviceIdle", requiresDeviceIdle ? 1 : 0);
         jobExtras.putInt("alwaysRunning", alwaysRunning ? 1 : 0);
+        jobExtras.putInt("allowedInForeground", allowedInForeground ? 1 : 0);
         if (alwaysRunning) {
             mJobBundle = new Bundle(jobExtras);
         } else {
