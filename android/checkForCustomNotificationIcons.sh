c42412d8a8eb6fba24973d9a2f67c93eb61f31c2
sagium <sagium2@gmail.com>
2018-12-10 22:03:40 +0200

    Code formatting

--- android/checkForCustomNotificationIcons.sh
+++ android/checkForCustomNotificationIcons.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-BUILD_DIR=$1
+BUILD_DIR="${1}/../src/main/res"
+ROOT_DIR="${2}/app/src/main/res"
 ICON_NAME="pilloxa_custom_notification.png"
 
 # Creating symlinks for the notification icons
 #
-ln -s ../../../../../../../android/app/src/main/res/drawable-mdpi/ICON_NAME \
-"${BUILD_DIR}/../src/main/res/drawable-mdpi/ICON_NAME"
+ln -s "${ROOT_DIR}/drawable-mdpi/${ICON_NAME}" \
+"${BUILD_DIR}/drawable-mdpi/${ICON_NAME}"
 
-ln -s ../../../../../../../android/app/src/main/res/drawable-hdpi/ICON_NAME \
-"${BUILD_DIR}/../src/main/res/drawable-hdpi/ICON_NAME"
+ln -s "${ROOT_DIR}/drawable-hdpi/${ICON_NAME}" \
+"${BUILD_DIR}/drawable-hdpi/${ICON_NAME}"
 
-ln -s ../../../../../../../android/app/src/main/res/drawable-xhdpi/ICON_NAME \
-"${BUILD_DIR}/../src/main/res/drawable-xhdpi/ICON_NAME"
+ln -s "${ROOT_DIR}/drawable-xhdpi/${ICON_NAME}" \
+"${BUILD_DIR}/drawable-xhdpi/${ICON_NAME}"
 
-ln -s ../../../../../../../android/app/src/main/res/drawable-xxhdpi/ICON_NAME \
-"${BUILD_DIR}/../src/main/res/drawable-xxhdpi/ICON_NAME"
+ln -s "${ROOT_DIR}/drawable-xxhdpi/${ICON_NAME}" \
+"${BUILD_DIR}/drawable-xxhdpi/${ICON_NAME}"
 
 # If the user has not any custom notification icons we remove the dangling symlinks
 # or else the build will fail
 #
-find -L "${BUILD_DIR}/../src/main/res" -type l -delete
+find -L "${BUILD_DIR}" -type l -delete
